c3ca2cfb472a67abb303ac8bfe0fbd76f3f5a396
Husni <husniwfayo@gmail.com>
2026-05-21 14:24:08 +0530

    changed the order of navlinks and content update

--- lib/navLinks.ts
+++ lib/navLinks.ts
@@ -1,7 +1,7 @@
 export const navLinks = [
     { name: "Conduct Research", href: "/conduct-research" },
-    { name: "About Us", href: "/about" },
     { name: "News", href: "/news" },
     { name: "Events", href: "/events" },
     { name: "Publications", href: "/publications" },
+    { name: "About Us", href: "/about" },
 ]
